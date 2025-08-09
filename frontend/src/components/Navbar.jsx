a43ca5a1c2d95d25ef02b189b7ce8e6bf7442422
Rajatkhanuja <rajatkhanuja81@gmail.com>
2025-08-09 20:31:22 +0530

    Initial commit or your custom message

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,72 +5,60 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
-  const closeMenu = () => {
-    setIsMenuOpen(false);
-  };
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+  const closeMenu = () => setIsMenuOpen(false);
 
   return (
-    <header className="bg-black fixed top-0 left-0 w-full z-50 shadow-md">
-      {/* Absolute Logo in Top-Left */}
-      <div className="absolute top-0 left-0">
+    <header className="bg-black text-white fixed top-0 left-0 w-full z-50 shadow-md">
+      <div className="max-w-[1300px] mx-auto flex justify-between items-center px-4 py-3">
+        {/* Logo */}
         <Link to="/" onClick={closeMenu}>
-          <img
-            src={logo}
-            alt="CarCraze Logo"
-            className="h-[50px] w-auto object-contain"
-          />
+          <img src={logo} alt="Car Craze Logo" className="h-12 w-auto" />
         </Link>
-      </div>
 
-      {/* Navbar Container */}
-      <nav className="flex justify-end items-center py-4 px-4 lg:px-8 pl-[70px]">
         {/* Desktop Menu */}
-        <div className="hidden lg:flex items-center gap-7 font-medium text-xl text-white">
-          <Link to="/" className="hover:text-yellow-400 transition duration-200 ease-linear">
+        <div className="hidden lg:flex items-center space-x-8 text-lg font-medium">
+          <Link to="/" className="hover:text-yellow-400 transition" onClick={closeMenu}>
             HOME
           </Link>
-          <Link to="/stock" className="hover:text-yellow-400 transition duration-200 ease-linear">
+          <Link to="/stock" className="hover:text-yellow-400 transition" onClick={closeMenu}>
             BUY USED CAR
           </Link>
-          <Link to="/sell" className="hover:text-yellow-400 transition duration-200 ease-linear">
+          <Link to="/sell" className="hover:text-yellow-400 transition" onClick={closeMenu}>
             SELL CAR
           </Link>
-          <Link to="/services" className="hover:text-yellow-400 transition duration-200 ease-linear">
+          <Link to="/services" className="hover:text-yellow-400 transition" onClick={closeMenu}>
             SERVICES
           </Link>
-          <Link to="/contact" className="hover:text-yellow-400 transition duration-200 ease-linear">
+          <Link to="/contact" className="hover:text-yellow-400 transition" onClick={closeMenu}>
             CONTACT US
           </Link>
         </div>
 
-        {/* Mobile Hamburger Icon */}
-        <div className="lg:hidden absolute top-6 right-4 flex items-center" onClick={toggleMenu}>
-          <button className="text-white bg-transparent border-none">
-            <i className={`fas ${isMenuOpen ? 'fa-times' : 'fa-bars'} text-2xl`}></i>
+        {/* Hamburger Icon */}
+        <div className="lg:hidden">
+          <button onClick={toggleMenu}>
+            <i className={`fas ${isMenuOpen ? "fa-times" : "fa-bars"} text-2xl`}></i>
           </button>
         </div>
-      </nav>
+      </div>
 
       {/* Mobile Dropdown Menu */}
       {isMenuOpen && (
-        <div className="lg:hidden bg-black text-white font-medium text-xl p-4 pl-8 pr-8 mt-2">
-          <Link to="/" onClick={closeMenu} className="block py-2 hover:text-yellow-400 transition">
+        <div className="lg:hidden bg-black text-white text-lg font-medium px-4 pb-4 space-y-2">
+          <Link to="/" onClick={closeMenu} className="block hover:text-yellow-400 transition">
             HOME
           </Link>
-          <Link to="/stock" onClick={closeMenu} className="block py-2 hover:text-yellow-400 transition">
+          <Link to="/stock" onClick={closeMenu} className="block hover:text-yellow-400 transition">
             BUY USED CAR
           </Link>
-          <Link to="/sell" onClick={closeMenu} className="block py-2 hover:text-yellow-400 transition">
+          <Link to="/sell" onClick={closeMenu} className="block hover:text-yellow-400 transition">
             SELL CAR
           </Link>
-          <Link to="/services" onClick={closeMenu} className="block py-2 hover:text-yellow-400 transition">
+          <Link to="/services" onClick={closeMenu} className="block hover:text-yellow-400 transition">
             SERVICES
           </Link>
-          <Link to="/contact" onClick={closeMenu} className="block py-2 hover:text-yellow-400 transition">
+          <Link to="/contact" onClick={closeMenu} className="block hover:text-yellow-400 transition">
             CONTACT US
           </Link>
         </div>
