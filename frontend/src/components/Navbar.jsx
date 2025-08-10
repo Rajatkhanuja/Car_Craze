9f6553be5e9ffa6c90232cc9a29957d47ad4a408
Rajatkhanuja <rajatkhanuja81@gmail.com>
2025-08-10 21:41:33 +0530

    Initial commit or your custom message

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [dropdownOpen, setDropdownOpen] = useState(false); // For mobile dropdown
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -12,7 +11,6 @@ const Navbar = () => {
 
   const closeMenu = () => {
     setIsMenuOpen(false);
-    setDropdownOpen(false);
   };
 
   return (
@@ -22,70 +20,38 @@ const Navbar = () => {
         {/* Logo */}
         <div className="flex items-center gap-2">
           <Link to="/" onClick={closeMenu} className="ml-0 sm:ml-0">
-            <img
-              src={logo}
-              alt="CarCraze Logo"
-              className="h-10 sm:h-12 ml-0"
+            <img 
+              src={logo} 
+              alt="CarCraze Logo" 
+              className="h-10 sm:h-12 ml-0" 
             />
           </Link>
         </div>
 
         {/* Desktop Menu */}
         <div className="hidden lg:flex items-center gap-7 font-medium text-xl text-white">
-          <Link
-            to="/about"
-            className="hover:text-yellow-400 transition duration-200 ease-linear no-underline text-white"
-          >
+          
+          <Link to="/about" className="hover:text-yellow-400 transition duration-200 ease-linear no-underline text-white">
             ABOUT US
           </Link>
-          <Link
-            to="/stock"
-            className="hover:text-yellow-400 transition duration-200 ease-linear no-underline text-white"
-          >
+          <Link to="/stock" className="hover:text-yellow-400 transition duration-200 ease-linear no-underline text-white">
             BUY USED CAR
           </Link>
-          <Link
-            to="/sell"
-            className="hover:text-yellow-400 transition duration-200 ease-linear no-underline text-white"
-          >
+          <Link to="/sell" className="hover:text-yellow-400 transition duration-200 ease-linear no-underline text-white">
             SELL CAR
           </Link>
-
-          {/* VALUE ADDED SERVICES - Desktop Dropdown */}
-          <div className="relative group">
-            <span className="hover:text-yellow-400 cursor-pointer text-white">
-              VALUE ADDED SERVICES
-            </span>
-            <div className="absolute hidden group-hover:block bg-[#1A1A1A] text-white mt-2 rounded shadow-lg z-50 min-w-[250px]">
-              <Link to="/services/insurance" className="block px-4 py-2 hover:bg-yellow-500">Insurance</Link>
-              <Link to="/services/roadside" className="block px-4 py-2 hover:bg-yellow-500">Road Side Assistance</Link>
-              <Link to="/services/warranty" className="block px-4 py-2 hover:bg-yellow-500">Warranty</Link>
-              <Link to="/services/pre-delivery" className="block px-4 py-2 hover:bg-yellow-500">Pre Delivery Inspection</Link>
-              <Link to="/services/painting" className="block px-4 py-2 hover:bg-yellow-500">Denting & Painting Work</Link>
-              <Link to="/services/dryclean" className="block px-4 py-2 hover:bg-yellow-500">Dryclean & Polish Work</Link>
-              <Link to="/services/rto" className="block px-4 py-2 hover:bg-yellow-500">RTO Work</Link>
-            </div>
-          </div>
-
-          <Link
-            to="/contact"
-            className="hover:text-yellow-400 transition duration-200 ease-linear no-underline text-white"
-          >
+          <Link to="/services" className="hover:text-yellow-400 transition duration-200 ease-linear no-underline text-white">
+             VALUE ADDED SERVICES
+          </Link>
+          <Link to="/contact" className="hover:text-yellow-400 transition duration-200 ease-linear no-underline text-white">
             CONTACT US
           </Link>
         </div>
 
         {/* Mobile Hamburger Icon */}
-        <div
-          className="lg:hidden absolute top-6 right-4 flex items-center"
-          onClick={toggleMenu}
-        >
+        <div className="lg:hidden absolute top-6 right-4 flex items-center" onClick={toggleMenu}>
           <button className="text-white bg-transparent border-none">
-            <i
-              className={`fas ${
-                isMenuOpen ? "fa-times" : "fa-bars"
-              } text-2xl`}
-            ></i>
+            <i className={`fas ${isMenuOpen ? 'fa-times' : 'fa-bars'} text-2xl`}></i>
           </button>
         </div>
       </nav>
@@ -93,60 +59,20 @@ const Navbar = () => {
       {/* Mobile Dropdown Menu */}
       {isMenuOpen && (
         <div className="lg:hidden bg-[#1A1A1A] text-white font-medium text-xl p-4 pl-8 pr-8 mt-2">
-          <Link
-            to="/about"
-            onClick={closeMenu}
-            className="block py-2 hover:text-yellow-400 transition duration-200 ease-linear no-underline text-white"
-          >
+          
+          <Link to="/about" onClick={closeMenu} className="block py-2 hover:text-yellow-400 transition duration-200 ease-linear no-underline text-white">
             ABOUT US
           </Link>
-          <Link
-            to="/stock"
-            onClick={closeMenu}
-            className="block py-2 hover:text-yellow-400 transition duration-200 ease-linear no-underline text-white"
-          >
+          <Link to="/stock" onClick={closeMenu} className="block py-2 hover:text-yellow-400 transition duration-200 ease-linear no-underline text-white">
             BUY USED CAR
           </Link>
-          <Link
-            to="/sell"
-            onClick={closeMenu}
-            className="block py-2 hover:text-yellow-400 transition duration-200 ease-linear no-underline text-white"
-          >
+          <Link to="/sell" onClick={closeMenu} className="block py-2 hover:text-yellow-400 transition duration-200 ease-linear no-underline text-white">
             SELL CAR
           </Link>
-
-          {/* VALUE ADDED SERVICES - Mobile Dropdown */}
-          <div>
-            <div
-              onClick={() => setDropdownOpen(!dropdownOpen)}
-              className="block py-2 hover:text-yellow-400 transition duration-200 ease-linear no-underline text-white cursor-pointer flex justify-between items-center"
-            >
-              <span>VALUE ADDED SERVICES</span>
-              <i
-                className={`fas fa-chevron-${
-                  dropdownOpen ? "up" : "down"
-                }`}
-              ></i>
-            </div>
-
-            {dropdownOpen && (
-              <div className="pl-4 text-base">
-                <Link to="/services/insurance" onClick={closeMenu} className="block py-1 hover:text-yellow-400">Insurance</Link>
-                <Link to="/services/roadside" onClick={closeMenu} className="block py-1 hover:text-yellow-400">Road Side Assistance</Link>
-                <Link to="/services/warranty" onClick={closeMenu} className="block py-1 hover:text-yellow-400">Warranty</Link>
-                <Link to="/services/pre-delivery" onClick={closeMenu} className="block py-1 hover:text-yellow-400">Pre Delivery Inspection</Link>
-                <Link to="/services/painting" onClick={closeMenu} className="block py-1 hover:text-yellow-400">Denting & Painting Work</Link>
-                <Link to="/services/dryclean" onClick={closeMenu} className="block py-1 hover:text-yellow-400">Dryclean & Polish Work</Link>
-                <Link to="/services/rto" onClick={closeMenu} className="block py-1 hover:text-yellow-400">RTO Work</Link>
-              </div>
-            )}
-          </div>
-
-          <Link
-            to="/contact"
-            onClick={closeMenu}
-            className="block py-2 hover:text-yellow-400 transition duration-200 ease-linear no-underline text-white"
-          >
+          <Link to="/services" onClick={closeMenu} className="block py-2 hover:text-yellow-400 transition duration-200 ease-linear no-underline text-white">
+            VALUE ADDED SERVICES
+          </Link>
+          <Link to="/contact" onClick={closeMenu} className="block py-2 hover:text-yellow-400 transition duration-200 ease-linear no-underline text-white">
             CONTACT US
           </Link>
         </div>
