0065b13dfdb3da06ec68f2063ac1ea2ee0579c57
Rajatkhanuja <rajatkhanuja81@gmail.com>
2025-08-12 13:47:40 +0530

    Initial commit or your custom message

--- backend/models/carModel.js
+++ backend/models/carModel.js
@@ -3,13 +3,14 @@ const mongoose = require('mongoose');
 const carSchema = new mongoose.Schema({
     name: { type: String, required: true },
     model: { type: String, required: true },
-    running: { type: String, required: true },
-    insurance: { type: String, required: true },
-    ownership: { type: String, required: true },
+    year: { type: Number, required: true }, // Reg. Year
     fuel: { type: String, required: true },
-    year: { type: Number, required: true },
-    registration: { type: String, required: true },
-    price: { type: Number, required: true },
+    running: { type: String, required: true }, // Km Driven
+    transmission: { type: String, required: true }, // ✅ New Field
+    ownership: { type: String, required: true },
+    insurance: { type: String, required: true },
+    registration: { type: String, required: true }, // Reg. No
+    price: { type: String, required: true }, 
     photo1: { type: String },
     photo2: { type: String },
     photo3: { type: String },
