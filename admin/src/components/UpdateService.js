1599f0ec7a4cd2d7fcbdb7ab1650401d32aeeef6
Rajatkhanuja <rajatkhanuja81@gmail.com>
2025-08-12 15:03:04 +0530

    Initial commit or your custom message

--- admin/src/components/UpdateService.js
+++ admin/src/components/UpdateService.js
@@ -79,7 +79,7 @@ const UpdateService = () => {
       <div>
         <Navbar />
         <div className="flex items-center justify-center min-h-screen">
-          <div className="text-xl">Loading...</div>
+          <div className="text-xl">Loading.........</div>
         </div>
       </div>
     );
