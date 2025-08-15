f4f2715ead705f9002f2ea4721042f7b31852796
Rajatkhanuja <rajatkhanuja81@gmail.com>
2025-08-15 16:13:21 +0530

    Initial commit or your custom message

--- frontend/src/pages/CarDetails.jsx
+++ frontend/src/pages/CarDetails.jsx
@@ -229,7 +229,7 @@ const CarDetails = () => {
                   <span> {stockCar.registration}</span>
                 </div>
                 <p className="car-card-price">
-                  {formatPrice(stockCar.price)}
+                    <span>{formatPrice(stockCar.price)} lakh</span>
                   <span className="other-charges">+ other charges</span>
                 </p>
               </div>
