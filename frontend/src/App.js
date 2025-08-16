388ca87126e1e03102c0dc1ea351276aecd2a03d
Rajatkhanuja <rajatkhanuja81@gmail.com>
2025-08-16 12:54:16 +0530

    Initial commit or your custom message

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Pages
@@ -22,10 +22,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const App = () => {
+
+  // ✅ Auto Refresh after 1 hour
+  useEffect(() => {
+    const refreshTimer = setTimeout(() => {
+      window.location.reload();
+    }, 3600000); // 1 hour = 3600000 milliseconds
+
+    return () => clearTimeout(refreshTimer); // Clean up on unmount
+  }, []);
+
   return (
     <>
       <BrowserRouter>
-        <ScrollToTop /> {/* ✅ Use ScrollToTop here */}
+        <ScrollToTop />
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
@@ -34,8 +44,6 @@ const App = () => {
           <Route path="/stock" element={<Stock />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/car-details/:id" element={<CarDetails />} />
-
-          {/* ✅ New Routes for Services Section */}
           <Route path="/buy-used-car" element={<BuyUsedCar />} />
           <Route path="/used-car-loan" element={<UsedCarLoan />} />
           <Route path="/sell-car" element={<SellCar />} />
